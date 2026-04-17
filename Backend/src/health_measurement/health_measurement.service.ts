cb77ad1ff0739a50b0d0bf92c36083bd637bd455
Mohsin <mohsinmanzoor32@gmail.com>
2026-04-17 15:20:48 +0500

    Health Measurements query sorted by DESC

--- Backend/src/health_measurement/health_measurement.service.ts
+++ Backend/src/health_measurement/health_measurement.service.ts
@@ -40,7 +40,7 @@ export class HealthMeasurementService {
                 'ai.anomaly_detected AS anomaly_detected'
             ])
             .where('hm.patient_id = :patient_id', { patient_id })
-            .orderBy('hm.created_at', 'ASC')
+            .orderBy('hm.created_at', 'DESC')
             .getRawMany();
 
         return rawResults.map(raw => {
